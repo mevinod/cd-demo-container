324dd04bd2802e2c5723991fec137ab0eebfa154
Vinod <34798214+mevinod@users.noreply.github.com>
2022-01-30 21:51:31 +0530

    Add PHPUnit via Composer

--- install/composer.sh
+++ install/composer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# Add PHPUnit via Composer
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
